cb1d2ee8ae389317e8a57ebd3fc225e0f209a004
Apollo <jackratermann@gmail.com>
2025-03-31 15:21:48 -0700

    yay

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,6 @@ if status is-interactive
 end
 
 alias hx="helix"
+alias vi="nvim"
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+set -gx PATH $HOME/.cabal/bin /home/apollo/.ghcup/bin $PATH # ghcup-env
