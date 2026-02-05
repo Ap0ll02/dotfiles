ae80b2ffe000b137cb3fd1ad2467d2877a0d7830
Ap0ll02 <jackratermann@gmail.com>
2026-02-04 16:03:11 -0800

    New Fish Update

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# --------------
+# ALIASES
+# --------------
 alias music="rmpc"
 alias lg="lazygit"
 alias vi="nvim"
@@ -9,21 +12,22 @@ alias ls="lla"
 alias peru="paru --bottomup"
 alias cal="khal calendar"
 alias cali="khal interactive"
+alias nv="neovide"
+alias code="neovide"
+alias sf="source ~/.config/fish/config.fish"
+alias sso="ssh olympus"
+
+# --------------
+# Shell and Other Inits
+# --------------
 starship init fish | source
 zoxide init fish | source
 
-if test -n "$SSH_CONNECTION"
-    ~/Scripts/ssh_welcome.sh
-end
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/apollo/.ghcup/bin $PATH # ghcup-env
-
-function riscv-env
-    fish_add_path /home/apollo/.dev/riscv/riscv-gnu-toolchain/inst/riscv32-unknown-elf/bin
-    echo "RISC-V toolchain activated"
-end
+# GHCUP Glasgow Haskell Compiler
+# set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/apollo/.ghcup/bin $PATH # ghcup-env
 
 fish_add_path -m /usr/bin /usr/local/bin
+
 # ZVM
 set -gx ZVM_INSTALL "$HOME/.zvm/self"
 set -gx PATH $PATH "$HOME/.zvm/bin"
